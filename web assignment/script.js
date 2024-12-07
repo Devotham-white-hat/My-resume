dc0b8924c0ed4c0dd19950bc40888374eab2cf41
Devotham-white-hat <userdev108@gmail.com>
2024-12-07 09:08:54 +0530

    Delete web assignment directory

--- web assignment/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // Example: Highlight current page link in navigation
-    $('nav ul li a').each(function() {
-        if (this.href == window.location.href) {
-            $(this).addClass('active');
-        }
-    });
-});
