b6ebf6c1f4714343f63edb91792e35eaa2e35c03
Devotham-white-hat <userdev108@gmail.com>
2024-12-09 19:25:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    var imageUrl = 'image.jpg';  // Replace with your image URL
-    $('#dynamicImage').html('<img src="' + imageUrl + '" alt="Dynamic Image">');
-});
